2e965ce7fd615f43872fc0e93dc258d591112108
just_pr()grammer <avazbekabdurafikov21@gmail.com>
2024-05-09 23:34:18 +0500

    add README.md and requirements.txt files

---
+++ README.md
@@ -0,0 +1,1 @@
+# This script send News in your email
--- sent_eamil.py
+++ sent_eamil.py
@@ -1,18 +1,14 @@
-import smtplib,ssl
-import os
+import smtplib, ssl
+
 
 def send_mail(message):
     host = "smtp.gmail.com"
     port = 465
-    username = "Your Password"
-    password = "Your app password from google"
+    username = ""
+    password = ""
     receiver = ""
     contex = ssl.create_default_context()
-#     message = """
-# Hi
-# How are you?
-# Bye!
-# """
+
     with smtplib.SMTP_SSL(host=host, port=port, context=contex) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
